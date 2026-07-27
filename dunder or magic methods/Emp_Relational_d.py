76c1a2fc3d7c24f44c85b44f332d6bf173271da0
nikhilbogawar <tejasnikhil72@gmail.com>
2026-07-27 12:45:41 +0530

    changes in Emp_Relational_d.py and bank_rel_d.py

--- dunder or magic methods/Emp_Relational_d.py
+++ dunder or magic methods/Emp_Relational_d.py
@@ -9,9 +9,13 @@ class Emp:
         return self.Id!=other.Id and self.Name!=other.Name
     def __hash__(self):
         return hash(self.Id)
+    def __str__(self):
+        return f"Id: {self.Id}, Name: {self.Name}"
+    def __repr__(self):
+        return self.__str__()
 e1=Emp(15,"Nikhil")
 e2=Emp(15,"Nikhil")
 print(e1==e2)  # True
 print(e1!=e2)  # False
 s={e1,e2}
-print(s)  # {<__main__.Emp object at 0x00000126FA5146E0>}
+print(s)  # {Id: 15, Name: Nikhil}
--- dunder or magic methods/bank_rel_d.py
+++ dunder or magic methods/bank_rel_d.py
@@ -5,7 +5,11 @@ class Bank:
         self.bal=0
     def __hash__(self):
         return hash(self.acc)
+    def __str__(self):
+        return f"Name: {self.Name}, Account: {self.acc}, Balance: {self.bal}"
+    def __repr__(self):
+        return self.__str__()
 b1=Bank("Nikhil",1543)
 b2=Bank("Nikhil",1875)
 k={b1,b2}
-print(k)  # {<__main__.Bank object at 0x000001E9099C8410>, <__main__.Bank object at 0x000001E9099D4590>}
+print(k)  # {Name: Nikhil, Account: 1875, Balance: 0, Name: Nikhil, Account: 1543, Balance: 0}
